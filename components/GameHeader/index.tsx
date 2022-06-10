9fa78d9731e7bf533c37148adab53ff29bb5c9af
Uvis Grinfelds <uvis.grinfelds@gmail.com>
2022-06-10 14:59:06 +0300

    Create Player Queue component

--- components/GameHeader/index.tsx
+++ components/GameHeader/index.tsx
@@ -5,9 +5,9 @@ import { useGame } from "../GameProvider";
 
 import Text from "../Text";
 import LogoMenu from "../LogoMenu";
-import UserAvatar from "../UserAvatar";
 import GameInventory from "../GameInventory";
 import { getCard } from "../../components/Cards";
+import PlayerQueue from "../PlayerQueue";
 
 export interface Props extends HTMLAttributes<HTMLElement> {
   palette?: "gradient";
@@ -23,7 +23,7 @@ const GameHeader: FC<Props> = ({
   loading,
   ...props
 }) => {
-  const { loggedIn, user } = useAuth();
+  const { user } = useAuth();
   const { gameState, players } = useGame();
   const [playersWithPoints, setPlayersWithPoints] = useState<Array<any>>([]);
   const [currentPlayer, setCurrentPlayer] = useState<any>("");
@@ -96,7 +96,7 @@ const GameHeader: FC<Props> = ({
             display: "flex",
             alignItems: "center",
             position: "relative",
-            zIndex: 1,
+            zIndex: 9999999,
           },
           palette === "gradient"
             ? {
@@ -168,108 +168,11 @@ const GameHeader: FC<Props> = ({
         >
           <GameInventory isOpponentsCards={true} cards={opponentsCards} />
         </div>
-        
 
-        <div css={{display: 'flex', justifyContent: 'space-between', minWidth: 180}}>
-        {loggedIn &&
-          user &&
-          playersWithPoints.map((player: any) => {
-            return (
-              <div
-                className="game-player"
-                key={player.userId}
-                css={{
-                  position: "relative",
-                  borderRadius: 9999,
-                  cursor: 'default',
-                  opacity: loadingDelayed ? '0' : '1',
-                  transform: loadingDelayed ?  'translate(1500px, 0)' : 'translate(0, 0)' ,
-                  "&::after": {
-                    opacity: 0,
-                    content: `'${player.points}'`,
-                    display: "flex",
-                    lineHeight: 3,
-                    transition: 'all 400ms',
-                    borderRadius: 9999,
-                    outline: "6px solid" + player.color,
-                    justifyContent: "center",
-                    alignItems: "center",
-                    fontSize: 60,
-                    fontFamily: 'Aldrich',
-                    position: "absolute",
-                    color: "#fff",
-                    background: player.color,
-                    zIndex: 9999,
-                    bottom: 0,
-                
-                    top: 0,
-                    left: 0,
-                    right: 0,
-                    pointerEvents: 'none',
 
-                  },
-                  "&::before": {
-                    opacity: 0,
-                    content: `'${player.username}'`,
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    transition: 'all 400ms',
-                    color: "black",
-                    fontFamily: 'Aldrich',
-                    position: "absolute",
-                    background: "#ffff",
-                    borderRadius: 6,
-                    lineHeight: 3,
+        <PlayerQueue loadingDelayed={loadingDelayed} playersWithPoints={playersWithPoints}  />
 
-          
-                    fontSize: 18,
-                    zIndex: 9999,
-                    bottom: -40,
-                    left: "50%",
-                    transform: "translate(-50%, 0)",
-                    padding: "12px 14px",
-                    paddingTop: 16,
-                    minWidth: 70,
-                    height: 30,
-                    pointerEvents: 'none',
-                    textTransform: 'uppercase'
-                  },
-                  "&:hover": {
-                    "&::after": {
-                      opacity: 1,
-                      fontSize: 42,
-                      pointerEvents: 'none',
-                      paddingTop: 10,
-                    },
-                    "&::before": {
-                      opacity: 1,
-                      pointerEvents: 'none',
-                      transform: "translate(-50%, 8px)",
 
-                    },
-                  },
-                }}
-              >
-                <UserAvatar
-                  css={{
-                    outline: "6px solid" + player.color,
-                    zIndex: 999999,
-                    "&:hover": {
-                      background: player.color,
-        
-                    },
-                  }}
-                  profilePictureUrl={
-                    player.profilePictureUrl
-                      ? player.profilePictureUrl
-                      : player.profileImageUrl
-                  }
-                />
-              </div>
-            );
-          })}
-          </div>
       </div>
     </header>
   );
